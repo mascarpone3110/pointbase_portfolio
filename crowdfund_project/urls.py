da16c3c21501ca07bef05e0ec5d1f200e245baed
mascarpone3110 <mascarpone31010@gmail.com>
2025-12-29 21:42:16 +0900

    update url.py

--- crowdfund_project/urls.py
+++ crowdfund_project/urls.py
@@ -2,8 +2,14 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
+
 
 urlpatterns = [
+    path("health/", health_check),
     path("admin/", admin.site.urls),
     path('api/', include('accounts.urls')),
     path('api/', include('points.urls'))
