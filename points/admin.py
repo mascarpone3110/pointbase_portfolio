0c2345802ad0db7134abb8414d641e60392ca158
kazuma <ki@MacBook-Pro.local>
2026-02-01 13:43:44 +0900

    Remove non-auth modules and keep authentication only

--- points/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Item, Order, OrderItem, PointManager, PointTransaction
-
-admin.site.register(Item)
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(PointManager)
-admin.site.register(PointTransaction)
